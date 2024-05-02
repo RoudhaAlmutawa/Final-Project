9a55fbd5cddb786db8e61f1360f1af79ce24c572
Roudha <alm.roudha@gmail.com>
2024-05-02 21:15:16 +0400

    added the class employee added the class Manager added the class Salesperson added the class Event added the class Client added the class Guest added the class Venue added the class Supplier added the class Caterer added the class CleaningCompany added the class DecorationsCompany added the class EntertainmentCompany added the class FurnitureSupplier added the GUI for managing event information added the test cases created the GUI added a reqirment in the GUI added the data handler

---
+++ Data Handler.py
@@ -0,0 +1,13 @@
+import pickle    # Importing the pickle module for serialization
+
+class DataManager:
+    def save_data(filename, data):
+        with open(filename, 'wb') as file:    # Open file in binary write mode
+            pickle.dump(data, file)     # Serialize and write data to the file
+
+    def load_data(filename):
+        try:
+            with open(filename, 'rb') as file:  # Open file in binary read mode
+                return pickle.load(file)    # Deserialize and return data from the file
+        except FileNotFoundError:
+            return []     # Return an empty list if the file is not found
