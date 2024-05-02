47ee9d53db972c157a268fe83a1e4f725d72a793
RoudhaAlmutawa <165319861+RoudhaAlmutawa@users.noreply.github.com>
2024-05-02 16:07:46 +0400

    Delete GUI Customer.py
    I will create another GUI

--- GUI Customer.py
+++
@@ -1,3 +0,0 @@
-import tkinter as tk
-from Classes import Employee, Manager, Salesperson, Event, Client, Guest, Venue, Supplier, Caterer, CleaningCompany, DecorationsCompany, EntertainmentCompany, FurnitureSupplier
-
